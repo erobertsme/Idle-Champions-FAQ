fe1427439eec7ec8a0796d11ea33e133d1e7c937
Archimedes <19399779+PJP2810@users.noreply.github.com>
2018-04-08 18:53:42 +0100

    Update questions.js
    Updated formatting for Q24

--- faq/questions.js
+++ faq/questions.js
@@ -118,7 +118,7 @@ questions: [
   {
     "id": "24",
     "question": "Where do I enter Chest Codes?",
-    "answer": "Chest Codes should be entered in game via: Shop (S)>Redeem a Code>Submit<br /><br />DLC codes are entered in Steam via: Games>Activate a Product on Steam...>Follow instructions on the window"
+    "answer": "Chest Codes should be entered in game via:<br /><ul><li>Shop (S)</li><li>Redeem a Code</li><li>Submit</li></ul><br /><br />DLC codes are entered in Steam via:<br /><ul><li>Games</li><li>Activate a Product on Steam...</li><li>Follow instructions on the window</li></ul>
   },
   {
     "id": "25",
